4f41c35077d95c98b3e102ffdb11f6a815dcb721
yonatan mengesha <mail2yonz@gmail.com>
2017-10-05 21:01:58 -0400

    Second commit

--- src/RandomGuessing.java
+++ src/RandomGuessing.java
@@ -1,30 +1,45 @@
 import java.util.Random;
 import java.util.Scanner;
 
+//This code guesses a random number and lets you know if your guess is equal or not
 public class RandomGuessing {
     public static void main(String[] args)
     {
-        Random rand= new Random ();
-        String ans;
+        Random rand= new Random();
+
         int randInt =1+ rand.nextInt (100);
         System.out.println(randInt);
-        output("Guess a number between 1 and a 100:");
+
+
+
         Scanner scan = new Scanner (System.in);
 
-        int guessedNum= scan.nextInt ();
+         do  {
+
+             output("Guess a number between 1 and a 100:");
+             int guessedNum= scan.nextInt ();
 
 
-            if(randInt==guessedNum)
-            {
-                output ( "Guessed Right" );
 
-             } else if (randInt< guessedNum)
-            {
-                output ( "Too high" );
-            } else if(randInt>guessedNum)
-            {
-                output("Too low");
-            }
+
+               if( randInt>guessedNum) {
+                   output("The number is Too low,Do you want to try again yes/no");
+               } else if (randInt< guessedNum)
+               {
+                   output ( "The number is Too high,Do you want to try again yes/no" );
+
+               } else if(randInt==guessedNum)
+               {
+                   output ( "Guessed Right!" );
+                    break;
+               }
+
+             output("Do you want to continue Yes or No");
+
+
+
+           }while (scan.next().equalsIgnoreCase("yes"));
+
 
 
     }
